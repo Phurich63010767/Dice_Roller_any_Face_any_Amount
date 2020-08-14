66bb88813bcae5f4e248e01cf70b330f8beaeb8f
Phurich63010767 <69179855+Phurich63010767@users.noreply.github.com>
2020-08-15 02:58:10 +0700

    Add files via upload

---
+++ Dice_Roller_any_Face_any_Amount/Source.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+
+using namespace std; 
+
+int main()
+
+{
+	START:
+	int x,y ;
+	cout << "Enter Dice's Face(s): ";
+	cin >> x; 
+	cout << endl;
+	cout << "How Many Dice?: ";
+	cin >> y;
+	cout << endl;
+	if (x < 1) 
+	{
+		cout << "Cannot Enter Below 1 Face or 1 Dice."<< endl;
+		system("Pause");
+		system("CLS");
+		goto START;
+	}
+	if (y < 1)
+	{
+		cout << "Cannot Enter Below 1 Face or 1 Dice."<< endl;
+		system("Pause");
+		system("CLS");
+		goto START;
+	}
+	else
+	{
+		 cout << "Result: ";
+		 int a;
+	     srand(time(0));
+			for (int i = 0; i < y; i++)
+			{			
+				a = (rand() % x) + 1;
+				cout << a << " ";	
+							 
+			}
+			cout << endl;
+			
+			
+	}
+
+	system("Pause");
+	return 0;
+}
